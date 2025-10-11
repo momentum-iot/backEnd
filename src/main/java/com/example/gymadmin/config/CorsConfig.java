d6a14e12da5b894c76ca18cdc74d135adaf6ae56
Carlos <U202015274@upc.edu.pe>
2025-10-11 00:44:31 -0500

    chore: added deployed frontend to CORS

--- src/main/java/com/example/gymadmin/config/CorsConfig.java
+++ src/main/java/com/example/gymadmin/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173"));
+        cfg.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173", "https://pumpup.netlify.app/"));
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         cfg.setAllowedHeaders(List.of("*"));
         cfg.setAllowCredentials(true);
