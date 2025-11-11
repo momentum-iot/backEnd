e100615c926cee79944198515aaeefe760c3286a
Carlos <U202015274@upc.edu.pe>
2025-11-11 16:08:24 -0500

    feat: class member deleted

--- src/main/java/com/example/gymadmin/repo/MemberRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.gymadmin.repo;
-
-import com.example.gymadmin.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface MemberRepo extends JpaRepository<Member, String> {
-    List<Member> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCaseOrEmailContainingIgnoreCase(String first, String last, String email);
-}
